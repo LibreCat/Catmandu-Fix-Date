14cbf99ad42eed0abf9fa9e19f1de897fe06d339
Nicolas Franck <nicolas.franck@ugent.be>
2014-11-13 15:35:52 +0100

    add license information to main pm

--- lib/Catmandu/Fix/Date.pm
+++ lib/Catmandu/Fix/Date.pm
@@ -28,6 +28,14 @@ Nicolas Franck, C<< <nicolas.franck at ugent.be> >>
   L<Catmandu::Fix>
   L<Catmandu>
 
+=head1 LICENSE AND COPYRIGHT
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
 =cut
 
 1;
